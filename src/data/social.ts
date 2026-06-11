e408cf8e1c6aef32d68d24268a6a303cb5df14f3
Yago Calomino <yagoferrocalomino@gmail.com>
2026-06-10 23:24:14 -0300

    fix: corrige url global do linkedin

--- src/data/social.ts
+++ src/data/social.ts
@@ -1,6 +1,6 @@
 export const SOCIAL = {
   github: "https://github.com/YagoCalomino",
-  linkedin: "https://www.linkedin.com/in/yagoferroca",
+  linkedin: "https://www.linkedin.com/in/yagoferrocalomino/",
   email: "mailto:yagoferrocalomino@gmail.com",
   resume: "/resume.pdf",
 } as const;
